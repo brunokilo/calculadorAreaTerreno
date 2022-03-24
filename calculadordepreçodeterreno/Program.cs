df5feb35d797612204cfeceab139697d6cd7fc9c
unknown <brunohenfp@gmail.com>
2022-03-24 14:12:28 -0300

    Primeiro commit

---
+++ calculadordepreçodeterreno/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace calculadordepreçodeterreno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double largura, comprimento, precoMetroQuadrado, area, preco;
+
+            Console.WriteLine("Largura do terreno: ");
+            largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Comprimento do terreno: ");
+            comprimento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Preço do metro quadrado: ");
+            precoMetroQuadrado = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            area = largura * comprimento;
+            preco = area * precoMetroQuadrado;
+
+            Console.WriteLine("Area = " + area.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Preço = " + preco.ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.ReadLine();
+        }
+    }
+}
